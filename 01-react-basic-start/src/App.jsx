714b01df6f24c4e70be2ddb9b1f759b908835a88
Alex Nagoryanskiy <leonelleo@yandex.ru>
2024-01-10 12:34:48 +0000

    add to gh

--- 01-react-basic-start/src/App.jsx
+++ 01-react-basic-start/src/App.jsx
@@ -1,9 +1,14 @@
-function App() {
+import Header from './components/Header'
+
+export default function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <Header />
+    <main>
+      <h1>Hello React!!</h1>
+    </main>
     </div>
   )
 }
 
-export default App
+
